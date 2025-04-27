d897511fc365b5da6b3982eac4757ec49bb102d2
J-ULY67 <nana.owusu-ansah@acity.edu.gh>
2025-04-27 00:53:43 +0000

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 
 // ✅ Allow multiple frontends for CORS
 const allowedOrigins = [
-  'https://backend-end-of-sem.onrender.com', 
+  'https://frontend-end-of-sem.onrender.com', 
   'https://j-uly67.github.io/Frontend-End-of-sem'                  
 ];
 
